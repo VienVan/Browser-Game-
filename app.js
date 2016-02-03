e67a87ed1ba1e07dde4103a5adbaaa81e5f18edb
Vien Van <vienvonvan@gmail.com>
2016-02-03 12:14:05 -0800

    Randomized word from ajax call

--- app.js
+++ app.js
@@ -7,13 +7,15 @@ var score = 0;
 var cream =
   'https://littlefilmlab.com/development/vientwist/wordlist.php?letters=bliss';
 
+
 //adds letter to board slot, push letter into an array, then join the letters to string
 $('.letters').click(function(event) {
   $('#slot1').append(this);
   slot.push($(this).attr('id'));
-  wordSubmitted = slot.join('');
-  $('.btn').css('margin-top', '20px');
+  joinWords();
+  adjustButton();
 })
+
 
 //testing to see if wordSubmitted is a word (in this case the correct 5 letter word)
 $('#submit').click(function() {
@@ -21,18 +23,14 @@ $('#submit').click(function() {
     alert('Winner Winner!');
   }
   getScore();
-  $('#score').html(score);
 });
 
 //twist function, to shuffle the letter in place
-$('#shuffle').click(function() {
-  var parent = $("#letterBoard");
-  var divs = parent.children();
-  while (divs.length) {
-    parent.append(divs.splice(Math.floor(Math.random() * divs.length), 1)[
-      0]);
-  }
-});
+$('#shuffle').click(shuffle);
+
+$('#clear').click(function() {
+  $('#slot1').replaceWith('#slot1');
+})
 
 //get the score of each word submitted,
 //**note** haven't gotten reset to work yet so can't add more than one score
@@ -46,11 +44,42 @@ function getScore() {
   } else {
     alert('Only words with three or more letters are allowed!');
   }
+  $('#score').html(score);
 }
 
+
+
+function adjustButton() {
+  return $('.btn').css('margin-top', '20px');
+}
+
+function joinWords() {
+  return wordSubmitted = slot.join('');
+}
+
+function shuffle() {
+  var parent = $("#letterBoard");
+  var divs = parent.children();
+  while (divs.length) {
+    parent.append(divs.splice(Math.floor(Math.random() * divs.length), 1)[
+      0]);
+  };
+}
 $.get(cream, function(data) {
   var letters = data.letters.split('');
-  for (var i = 0; i < letters.length; i++) {
-    $('.letters').html(letters[i]);
-  }
+
+  $('.letters').each(function(index) {
+    $(this).attr('id', letters[index]);
+    $(this).html(letters[index]);
+  })
+
+  // while(letters.length > 0) {
+  //   var i = Math.floor(Math.random() letters.length);
+  //
+  // }
+  // for (var i = 0; i < letters.length; i++) {
+  //   $('.letters').html(letters[i]);
+  //   // $('.letters').attr('id', i);
+  //   // console.log($('.letters'));
+  // }
 });

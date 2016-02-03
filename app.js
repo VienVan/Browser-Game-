a27f9897275529f81c52c1339625a47f08f43f21
Vien Van <vienvonvan@gmail.com>
2016-02-02 19:54:49 -0800

    joined slot array into one word

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ console.log('linked');
 var word = 'cream';
 var threeLetters = ['ace', 'car', 'are', 'era', 'arc', 'ear'];
 var slot = [];
-var answers = $('#answer').val();
+var wordSubmitted;
 //
 // function isThreeLetters() {
 //   for (var i = 0; i < threeLetters.length; i++) {
@@ -12,10 +12,21 @@ var answers = $('#answer').val();
 //   }
 // }
 
+
 $('.letters').click(function(event) {
   $('#slot1').prepend(this);
   slot.push($(this).attr('id'));
-  console.log(slot);
+  wordSubmitted = slot.join('');
+  console.log(wordSubmitted);
+
 })
+
+function isThreeLetters() {
+  for (var i = 0; i < slot.length; i++) {
+    if (slot.join('') === threeLetters[i]) {
+      return threeLetters[i];
+    }
+  }
+}
 console.log($('#a').attr('class'));
 // isThreeLetters();

c7fe72026c4686fa9cdcdc226376a7a06239c9af
Vien Van <vienvonvan@gmail.com>
2016-02-02 23:31:03 -0800

    styled page, added AJAX to access word bank, added margin-top to buttons

--- app.js
+++ app.js
@@ -1,26 +1,30 @@
 console.log('linked');
 var word = 'cream';
-var threeLetters = ['ace', 'car', 'are', 'era', 'arc', 'ear'];
+var threeLetters = ['ace', 'car', 'are', 'era', 'arc', 'ear']; //test
 var slot = [];
 var wordSubmitted;
 var score = 0;
+var cream =
+  'https://littlefilmlab.com/development/vientwist/wordlist.php?letters=bliss';
 
-
-
+//adds letter to board slot, push letter into an array, then join the letters to string
 $('.letters').click(function(event) {
-  $('#slot1').prepend(this);
+  $('#slot1').append(this);
   slot.push($(this).attr('id'));
   wordSubmitted = slot.join('');
+  $('.btn').css('margin-top', '20px');
 })
 
+//testing to see if wordSubmitted is a word (in this case the correct 5 letter word)
 $('#submit').click(function() {
   if (wordSubmitted === word) {
     alert('Winner Winner!');
   }
   getScore();
-  console.log(score);
+  $('#score').html(score);
 });
 
+//twist function, to shuffle the letter in place
 $('#shuffle').click(function() {
   var parent = $("#letterBoard");
   var divs = parent.children();
@@ -30,6 +34,8 @@ $('#shuffle').click(function() {
   }
 });
 
+//get the score of each word submitted,
+//**note** haven't gotten reset to work yet so can't add more than one score
 function getScore() {
   if (wordSubmitted.length === 3) {
     score += 300;
@@ -41,3 +47,10 @@ function getScore() {
     alert('Only words with three or more letters are allowed!');
   }
 }
+
+$.get(cream, function(data) {
+  var letters = data.letters.split('');
+  for (var i = 0; i < letters.length; i++) {
+    $('.letters').html(letters[i]);
+  }
+});

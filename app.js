b2212776b9f5a669d3c7040906710f3a64ec7469
Vien Van <vienvonvan@gmail.com>
2016-02-04 17:14:36 -0800

    Prevented submitted words to append

--- app.js
+++ app.js
@@ -2,94 +2,111 @@ console.log('linked');
 var slot = [];
 var letters = [];
 var answeredWords = [];
-var word = 'cream';
 var wordSubmitted;
 var score = 0;
-var cream =
+var url =
   'https://littlefilmlab.com/development/vientwist/wordlist.php?letters=';
 var message = 'Good Job Trick!'
 // var slot2 = [];
+var submittedCheck = 0;
 
 
 
-
-
-$.get(cream, function(data) {
-    globalData = data;
-    var letters = data.letters.split('');
-    var wordList = data.word_list;
-  $('.letters').each(function(index) {
-    $(this).attr('id', letters[index]);
-    $(this).html(letters[index]);
-  });
-
-  $('#submit').click(function() {
-
-    $.playSound('clickSound');
-    $('#alertMessage').html(message);
-
-    if($.inArray(wordSubmitted, wordList) !== -1) {
-        if(wordSubmitted.length === 5) {
-          $('#alertMessage').html('Congratulations! You can move on to the next round.');
-        }
-            getScore();
-            $('#answers').append('<p>' + wordSubmitted + '</p>')
-            submitWords();
-
-    } else if($.inArray(wordSubmitted, wordList) === -1) {
-      $('#alertMessage').html('NOT A WORD');
-    }
-
-  });
-
-});
-
-function selectLetter(){
-  $('.letters').click(function() {
-    $('#slot1').append($(this));
-    slot.push($(this).text());
-    letters.push(this);
-    // console.log("this is:", this);
-    // console.log("slot contains:", slot);
-    // console.log("letters are:", letters[0]);
-    joinWords();
-    adjustButton();
-    $.playSound('clickSound');
-
-  });
-};
+callData();
 selectLetter();
 
-//twist function, to shuffle the letter in place
-$('#shuffle').click(shuffle);
+
+function callData() {
+  $.get(url, function(data) {
+
+      var letters = data.letters.split('');
+      var wordList = data.word_list;
+
+    $('.letters').each(function(index) {
+      $(this).attr('id', letters[index]);
+      $(this).html(letters[index]);
+    });
+
+    $('#submit').click(function() {
+          isSubmitted();
+          $.playSound('clickSound');
+
+          if( submittedCheck === 1){
+              $('#alertMessage').html(message);
+
+                  if($.inArray(wordSubmitted, wordList) !== -1) {
+
+                      if(wordSubmitted.length === 5) {
+                          $('#alertMessage').html('Congratulations! You can move on to the next round.');
+                        }
+                        getScore();
+                        $('#answers').append('<p>' + wordSubmitted + '</p>')
+                        submitWords();
+
+                  } else if($.inArray(wordSubmitted, wordList) === -1) {
+                          $('#alertMessage').html('NOT A WORD');
+                        }
+
+          } else {
+              submittedCheck = 0;
+            }
+
+    });
+
+  });
+}
+
+
+function selectLetter(){
+    $('.letters').click(function() {
+          $('#slot1').append($(this));
+          slot.push($(this).text());
+          letters.push(this);
+          joinWords();
+          adjustButton();
+          $.playSound('clickSound');
+
+        });
+};
+
+
+    $('#shuffle').click(shuffle);
+
+    $('body').keypress(function(args) {
+        if (args.which === 13) {
+            $("#submit").click();
+        } else if (args.which === 32) {
+          $('#shuffle').click();
+        }
+    });
+
+    $('body').keydown(function(args) {
+      if (args.which === 8) {
+        $('#clear').click();
+      } else if (args.which === 79) {
+        alert("o");
+      }
+    });
+
 
 $('#clear').click(function() {
-  $('#slot1').empty();
-  letters.forEach(function(element) {
-    $('#letterBoard').append(element);
-  })
-  // console.log(letters);
-   // append each DOM element in the array
-  slot = [];
-  letters = [];
-  wordSubmitted = '';
-  var FOO = $('.letters');
-    for (var i = 0; i < FOO.length; i++) {
-      // console.log(FOO[i])
-      $(FOO[i]).off('click');
-    }
-  selectLetter();
-  $.playSound('clickSound');
+      $('#slot1').empty();
+      letters.forEach(function(element) {
+        $('#letterBoard').append(element);
+      })
+      slot = [];
+      letters = [];
+      wordSubmitted = '';
+      var FOO = $('.letters');
+        for (var i = 0; i < FOO.length; i++) {
+            $(FOO[i]).off('click');
+        }
+      selectLetter();
+      $.playSound('clickSound');
 
-  // $('.letters').on('click', function() {
-  //   $('#slot1').append(this);
-  // });
 });
 
 
-
-//get the score of each word submitted,
-//**note** haven't gotten reset to work yet so can't add more than one score
 function getScore() {
   if (wordSubmitted.length === 3) {
     score += 300;
@@ -105,37 +122,39 @@ function getScore() {
 }
 
 
-function addWordToPage() {
-
-}
 function adjustButton() {
-  return $('.btn').css('margin-top', '20px');
+    return $('.btn').css('margin-top', '20px');
 }
-// function pushToArray(a) {
-//   slot.push($(a).html())
-// }
 
 function submitWords() {
-  answeredWords.push(wordSubmitted);
+    answeredWords.push(wordSubmitted);
+    sessionStorage['answers'] = answeredWords;
 }
 
+function isSubmitted() {
+  if($.inArray(wordSubmitted, answeredWords) !== -1) {
+    $('#alertMessage').html('You have already guessed this word!');
+    submittedCheck = 0;
+  } else {
+    submittedCheck = 1;
+  }
+};
+
 function joinWords() {
-  // localStorage.setItem('wordSubmitted', slot.join(''));
-  return wordSubmitted = slot.join('');
-  // return wordSubmitted = localStorage['wordSubmitted'];
+    return wordSubmitted = slot.join('');
+
 }
 
 
 function shuffle() {
-  var parent = $("#letterBoard");
-  var divs = parent.children();
-  while (divs.length) {
+    var parent = $("#letterBoard");
+    var divs = parent.children();
+    while (divs.length) {
     parent.append(divs.splice(Math.floor(Math.random() * divs.length), 1)[
       0]);
-  };
-  $.playSound('clickSound');
-  $('#letterBoard').mixItUp();
-
+    };
+    $.playSound('clickSound');
+    $('#letterBoard').mixItUp();
 }
 
 
@@ -151,34 +170,3 @@ $.extend({
       ).appendTo('body');
     }
   });
-
-
-$('body').keypress(function(args) {
-    if (args.keyCode == 13) {
-        $("#submit").click();
-        return false;
-    } else if (args.keyCode == 32) {
-      $('#shuffle').click();
-
-    }
-});
-// function haveSubmitted() {
-//   if($.inArray(wordSubmitted, answeredWords) !== -1) {
-//     alert('You have already entered this word!');
-//   }
-// }
-
-//
-// $(document).ready(function() {
-//   bindEvents();
-//   startGame();
-// })
-//
-// bindEvents() {
-//   listenerA()
-//   listenerB()
-// }
-//
-// startGame() {
-//
-// }
